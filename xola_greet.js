b44731311ce8a5215e794664a34b2a4e6ba719f9
Loveness <lchinake997@gmail.com>
2022-08-23 17:03:30 +0200

    new additiions

--- xola_greet.js
+++ xola_greet.js
@@ -1,6 +1,16 @@
 import chalk from 'chalk';
 //import greet module that is in current folder
 import greet from './greet.js'
-
 const styledMessage = chalk.bgGreen.black(greet('Xola'))
 console.log(styledMessage)
+
+import fun from 'everyday-fun'
+import figlet from 'figlet'
+figlet('The Riddler', function(err, data){
+    console.log(data)
+    const teaser = fun.getRandomRiddle()
+console.log(chalk.black.bgGreen(teaser.riddle))
+setTimeout (function(){
+    console.log(chalk.white.bgRed(teaser.answer));
+}, 5000)
+})
